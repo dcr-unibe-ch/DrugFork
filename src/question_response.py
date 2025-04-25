96a4fb3b5237c42629519e0ffbe558444ab16d65
shtosti <hanna.hubarava@gmail.com>
2025-04-25 16:04:06 +0200

    create questins-response pairs in a dict

---
+++ src/question_response.py
@@ -0,0 +1,86 @@
+question_response_pairs = {
+    "Origin": {
+        "question": "What is the authorisation body which performs drug approval?",
+        "response": "`Origin` (string)"
+    },
+    "Marketing_authorisation_number": {
+        "question": "What is the marketing authorisation / document number?",
+        "response": "`Marketing_authorisation_number` (string)"
+    },
+    "Drug": {
+        "question": "What is the market name of the drug?",
+        "response": "`Drug` (string)"
+    },
+    "Non_proprietary_name": {
+        "question": "What is the non-proprietary name of the drug (name of the substance)?",
+        "response": "`Non_proprietary_name` (string)"
+    },
+    "Drug_class": {
+        "question": "What is the drug class? Choose from the following: `Small molecule`, `Biologics`, `Peptides and proteins`, `Cell and gene therapy`, `Vaccine`, or else `Other`",
+        "response": "`Drug_class` (string)"
+    },
+    "Pharmaceutical_form": {
+        "question": "What is the pharmaceutical form of the drug (application form)?",
+        "response": "`Pharmaceutical_form` (string)"
+    },
+    "Administration_route": {
+        "question": "How is the drug administered to the patient?",
+        "response": "`Administration_route` (string)"
+    },
+    "Decision": {
+        "question": "What is the decision of the authorisation body? Choose from the following: `approved`, `temporary authorisation`, `withdrawn`, `refused`",
+        "response": "`Decision` (string)"
+    },
+    "Current_status": {
+        "question": "What is the current status of the drug? Choose from the following: `authorised`, `authorised (under additional monitoring)`, `withdrawn`, `NA`",
+        "response": "`Current_status` (string)"
+    },
+    "Decision_date": {
+        "question": "What is the date of the decision? Format: dd.mm.yyyy",
+        "response": "`Decision_date` (string)"
+    },
+    "Orphan_drug_status": {
+        "question": "Is the drug designated as an orphan drug? Choose from the following: `yes`, `no`",
+        "response": "`Orphan_drug_status` (string)"
+    },
+    "Indication_requested": {
+        "question": "What is the indication requested for the drug?",
+        "response": "`Indication_requested` (string)"
+    },
+    "Indication_approved": {
+        "question": "What is the indication approved for the drug?",
+        "response": "`Indication_approved` (string)"
+    },
+    "Disease_class(es)": {
+        "question": "What is the disease class of the drug?",
+        "response": "`Disease_class(es)` (string)"
+    },
+    "Application_date": {
+        "question": "What is the date of the application for drug approval? Format: dd.mm.yyyy",
+        "response": "`Application_date` (string)"
+    },
+    "Nonclinical_pharmacology_species": {
+        "question": "What is the species used for nonclinical pharmacology? Look under non-clinical aspects: pharmacology. Format: use singular form of the species name. For example, if the species is `mouse`, use `mouse` instead of `mice`. If there are multiple species, separate them with a comma. For example, `mouse, rat, hamster`.",
+        "response": "`Nonclinical_pharmacology_species` (string)"
+    },
+    "Nonclinical_pharmacology_strain": {
+        "question": "What is the strain used for nonclinical pharmacology? Look under non-clinical aspects: pharmacology. Format: strain + species name in singular form. If there are multiple strains, separate them with a comma. For example, `baboon monkey, cotton rat, NSG mouse`.",
+        "response": "`Nonclinical_pharmacology_strain` (string)"
+    },
+    "Nonclinical_pharmacology_model": {
+        "question": "What is the model used for nonclinical pharmacology? Look under non-clinical aspects: pharmacology. Format: model + species name in singular form. `NA` if not reported. If there are multiple models, separate them with a comma.",
+        "response": "`Nonclinical_pharmacology_model` (string)"
+    },
+    "Nonclinical_pharmacokinetics_species": {
+        "question": "What is the species used for nonclinical pharmacokinetics? Look under non-clinical aspects: pharmacokinetics. Format: use singular form of the species name. For example, if the species is `mouse`, use `mouse` instead of `mice`. If there are multiple species, separate them with a comma. For example, `mouse, rat, hamster`.",
+        "response": "`Nonclinical_pharmacokinetics_species` (string)"
+    },
+    "Nonclinical_pharmacokinetics_strain": {
+        "question": "What is the strain used for nonclinical pharmacokinetics? Look under non-clinical aspects: pharmacokinetics. Format: strain + species name in singular form. If there are multiple strains, separate them with a comma. For example, `baboon monkey, cotton rat, NSG mouse`.",
+        "response": "`Nonclinical_pharmacokinetics_strain` (string)"
+    },
+    "Nonclinical_pharmacokinetics_model": {
+        "question": "What is the model used for nonclinical pharmacokinetics? Look under non-clinical aspects: pharmacokinetics. Format: model + species name in singular form. `NA` if not reported. If there are multiple models, separate them with a comma.",
+        "response": "`Nonclinical_pharmacokinetics_model` (string)"
+    }
+}
