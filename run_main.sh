e250d6ffcb314140abfc58ad6f00111c4949805f
shtosti <hanna.hubarava@gmail.com>
2025-04-25 16:34:36 +0200

    run generation

--- run_main.sh
+++ run_main.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 # ===========================================================
-FILE_NAME="SwissPAR_Levocalm.pdf"
+FILE_LIST="data/random/SwissPar_5.txt"
 # ==========================================================
 
 DATA_DIR="./data/SwissPar/SwissPAR_Jan19_2025"
-FILE_PATH="$DATA_DIR/$FILE_NAME"
 MODEL="gpt-4o-mini"
 SAVE_DIR="./output"
 TEMPERATURE=0.1
 MAX_TOKENS=300
 
 mkdir -p "$SAVE_DIR"
-# Check if the file exists
-if [ ! -f "$FILE_PATH" ]; then
-    echo "File not found!"
-    exit 1
-fi
 
 python ./src/main.py\
-    --filepath "$FILE_PATH" \
-    --file_name "$FILE_NAME" \
+    --file_list "$FILE_LIST" \
+    --data_dir "$DATA_DIR" \
     --model "$MODEL" \
     --save_dir "$SAVE_DIR" \
     --temperature "$TEMPERATURE" \
