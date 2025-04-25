47bd9f0a5f1977f98b891dbfcc498fe1bbff3810
shtosti <hanna.hubarava@gmail.com>
2025-04-25 15:37:17 +0200

    create main

---
+++ src/main.py
@@ -0,0 +1,97 @@
+from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
+import json
+import argparse
+from jsonschema import validate, ValidationError
+from openai import OpenAI
+from schema import json_schema
+
+
+def load_env_variables():
+    """Load environment variables from .env file."""
+    load_dotenv(override=True)
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    GOOGLEAI_API_KEY = os.getenv("GOOGLEAI_API_KEY")
+    return OPENAI_API_KEY, GOOGLEAI_API_KEY
+
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()  
+    parser.add_argument("--filepath", type=str, help="Path to the input file", required=True)
+    parser.add_argument("--file_name", type=str, help="Name of the input file", required=True)
+    parser.add_argument("--model", type=str, default="gpt-4o-mini", help="Model name", required=False)
+    parser.add_argument("--save_dir", type=str, help="Path to the output dir", required=False)
+    parser.add_argument("--temperature", type=float, default=0.7, help="Temperature for the model")
+    parser.add_argument("--max_tokens", type=int, default=300, help="Max tokens for the model")
+    return parser.parse_args()
+
+def handle_file(file_path):
+    if file_path.endswith('.pdf'):
+        reader = PdfReader(file_path)
+        text = "".join([page.extract_text() or '' for page in reader.pages])
+    else:
+        return "Unsupported file type. Please upload a PDF.", None
+    return text
+
+def generate_response(text, client, model_name, file_name):
+
+    system_role = "You are a helpful expert in Swiss drug approval processes"
+    user_prompt = f"""
+        You are going to read a drug approval report. Read the text attentively and answer the following questions in the specified json format with the specified keys:\n\
+        1) Question: What is the drug name?\n\
+        2) What is the substance name?\n\
+        3) What year was the decision made?\n\n\
+        Generate only the answers to the above questions, in the correct order. Be as concise as possible.\n\
+        TEXT:\n{text}\n\n
+        """ 
+
+    output_schema = """
+        Generate a response in JSON format with the following keys:\
+        - `drug_name` (string)\n\
+        - `substance_name` (string)\n\
+        - `year_of_decision` (integer)\n\n\
+        Your response should contain these keys and only these keys, with appropriate values based on the report you're analyzing."""
+    prompt = user_prompt + output_schema
+
+    """Generate a response from the OpenAI API."""
+    try:
+        response = client.chat.completions.create(
+            model=model_name,
+            messages=[
+                {"role": "system", "content": system_role},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=0.1,
+            max_tokens=300
+        )
+        response_content = response.choices[0].message.content.strip()
+        if response_content.startswith("```json") and response_content.endswith("```"):
+            response_content = response_content[7:-3].strip()
+        try:
+            response_json = json.loads(response_content)
+            
+            validate(instance=response_json, schema=json_schema)
+            return {file_name: response_json}
+        except json.JSONDecodeError:
+            return {file_name: f"Error: The response is not valid JSON: {response_content}"}
+        except ValidationError as ve:
+            return {file_name: f"Error: The response does not match the expected schema: {ve.message}"}
+    except Exception as e:
+        return {file_name: f"Error generating response: {str(e)}"}
+
+def main():
+    args = parse_arguments()
+    OPENAI_API_KEY, _ = load_env_variables()
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    text = handle_file(args.filepath)
+    if text:
+        response = generate_response(text, client, model_name=args.model, file_name=args.file_name)
+        print(response)
+    else:
+        raise Exception("No text extracted from the file.")
+
+
+
+if __name__ == "__main__":
+    main()
