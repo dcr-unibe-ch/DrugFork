73a9c6c3e013c98f9fc049583eb2dad47c1c7214
shtosti <hanna.hubarava@gmail.com>
2025-05-22 14:02:20 +0200

    create new pipe to create randomized data subsets

---
+++ run_randomize_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ==========================================================
+# TODO change the below arguments as needed
+NUM_SAMPLES=5
+DATASET="SwissPar" # options: EMA, SwissPar
+# ==========================================================
+
+
+if [ "$DATASET" == "EMA" ]; then
+    DATA_DIR="data/EMA/EMA_downloads"
+elif [ "$DATASET" == "SwissPar" ]; then    
+    DATA_DIR="data/SwissPar/SwissPAR_Jan19_2025"
+fi
+
+python src/randomize_data.py\
+    --dataset "$DATASET" \
+    --data_dir "$DATA_DIR" \
+    --num_samples "$NUM_SAMPLES" \
+    --save_dir "./data/randomized_data" \
+    --seed "42"
