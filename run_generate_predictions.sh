3ed60637f6cd93609957b92cc0fe5a7d52d51edb
shtosti <hanna.hubarava@gmail.com>
2025-06-12 19:28:01 +0200

    format

---
+++ run_generate_predictions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ==========================================================
+# TODO change the below arguments as needed
+FILE_LIST="data/EMA/EMA_list.txt"
+DATASET="EMA" # Options: EMA, SwissPar
+# ==========================================================
+
+
+if [ "$DATASET" == "EMA" ]; then
+    DATA_DIR="data/EMA/EMA_downloads"
+elif [ "$DATASET" == "SwissPar" ]; then    
+    DATA_DIR="data/SwissPar/SwissPAR_Jan19_2025"
+fi
+
+SAVE_DIR="./output"
+mkdir -p "$SAVE_DIR"
+
+python ./src/generate_predictions.py\
+    --file_list "$FILE_LIST" \
+    --data_dir "$DATA_DIR" \
+    --model "gpt-4o-mini" \
+    --save_dir "./output" \
+    --temperature "0.1" \
+    --max_tokens "1000" \
+    --dataset "$DATASET" 
