4e226c7ec4aa16216e2ef3df0a5f636dbd1c1a09
shtosti <hanna.hubarava@gmail.com>
2025-07-15 13:15:52 +0200

    modify schema

--- src/schema.py
+++ src/schema.py
@@ -1,10 +1,10 @@
 json_schema = {
     "type": "object",
     "properties": {
-        "Origin": {"type": "string"},
         "Marketing_authorisation_number": {"type": "string"},
-        "Drug": {"type": "string"},
+        "Drug_name": {"type": "string"},
         "Non_proprietary_name": {"type": "string"},
+        "Marketing_authorisation_holder": {"type": "string"},
         "Drug_class": {"type": "string"},
         "Pharmaceutical_form": {"type": "string"},
         "Administration_route": {"type": "string"},
@@ -12,19 +12,12 @@ json_schema = {
         "Current_status": {"type": "string"},
         "Decision_date": {"type": "string"},
         "Orphan_drug_status": {"type": "string"},
-        "Indication_requested": {"type": "string"},
+        "Indication_extended": {"type": "string"},
         "Indication_approved": {"type": "string"},
         "Disease_class(es)": {"type": "string"},
         "Application_date": {"type": "string"},
-        "Nonclinical_pharmacology_species": {"type": "string"},
-        "Nonclinical_pharmacology_strain": {"type": "string"},
-        "Nonclinical_pharmacology_model": {"type": "string"},
-        "Nonclinical_pharmacokinetics_species": {"type": "string"},
-        "Nonclinical_pharmacokinetics_strain": {"type": "string"},
-        "Nonclinical_pharmacokinetics_model": {"type": "string"},
-        "Nonclinical_toxicology_species": {"type": "string"},
-        "Nonclinical_toxicology_strain": {"type": "string"},
-        "Nonclinical_toxicology_model": {"type": "string"}
+        "Nonclinical_abridged": {"type": "string"},
+        "Referral_body": {"type": "string"}
     },
-    "required": ["Origin", "Marketing_authorisation_number", "Drug", "Non_proprietary_name", "Drug_class", "Pharmaceutical_form", "Administration_route", "Decision", "Current_status", "Decision_date", "Orphan_drug_status", "Indication_requested", "Indication_approved", "Disease_class(es)", "Application_date", "Nonclinical_pharmacology_species", "Nonclinical_pharmacology_strain", "Nonclinical_pharmacology_model", "Nonclinical_pharmacokinetics_species", "Nonclinical_pharmacokinetics_strain", "Nonclinical_pharmacokinetics_model", "Nonclinical_toxicology_species", "Nonclinical_toxicology_strain", "Nonclinical_toxicology_model"]
+    "required": ["Marketing_authorisation_number", "Drug_name", "Non_proprietary_name", "Marketing_authorisation_holder", "Drug_class", "Pharmaceutical_form", "Administration_route", "Decision", "Current_status", "Decision_date", "Orphan_drug_status", "Indication_extended", "Indication_approved", "Disease_class(es)", "Application_date", "Nonclinical_abridged", "Referral_body"]
 }
