89ff6f65a3e946a46bbbf12c941f20ff4b219df7
shtosti <hanna.hubarava@gmail.com>
2025-07-17 16:58:20 +0200

    run eval on japan

--- run_evaluate.sh
+++ run_evaluate.sh
@@ -3,14 +3,13 @@
 # ==========================================================
 # TODO change the below argument as needed
 
-INPUT_FILE="20250717_Japan_gpt-4o_assessed.json.csv"
-# remove the .csv extension if not needed from the name
+INPUT_FILE="20250717_Japan_gpt-4o_assessed.csv"
 
 # ==========================================================
 
 
 python src/evaluate.py\
     --input_file "./evaluation/processed_files/${INPUT_FILE}" \
-    --output_file "./evaluation/results_${INPUT_FILE}.json"
+    --output_file "./evaluation/processed_files/results_${INPUT_FILE}.json"
 
 echo "Evaluation results saved to ./evaluation/results_${INPUT_FILE}.json"
