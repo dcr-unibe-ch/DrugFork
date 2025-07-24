479b159c105eec86b4e832f23460855c796c5773
shtosti <hanna.hubarava@gmail.com>
2025-07-24 10:13:45 +0200

    add bash script to run combining jsons

---
+++ run_combine_eval_inference.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# =================================================
+# TODO
+DATASET="SWISSMEDIC" # EMA, JAPAN, AUSTRALIA, SWISSMEDIC
+EVAL="evaluation\output\20250717_SwissMedic_gpt-4o.json"
+INFERENCE="output\20250722_SwissMedic_gpt-4o.json"
+# ==================================================
+
+
+# Run the Python script with the specified arguments
+python src/combine_eval_inference.py \
+    --input_file_1 "$EVAL" \
+    --input_file_2 "$INFERENCE" \
+    --output_file "inference/combined/$DATASET.json"
+
+# Print a message indicating where the results are saved
+echo "Combined evaluation results saved to inference/combined/$DATASET.json"
