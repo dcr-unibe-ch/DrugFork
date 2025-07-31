851638de9745ec0af11ddc60b3d2e9e799cad933
shtosti <hanna.hubarava@gmail.com>
2025-07-31 11:59:33 +0200

    generate files with extracted data

--- run_extract_from_columns.sh
+++ run_extract_from_columns.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 # ==========================================================
 # TODO change the below arguments as needed
-INPUT_FILE="inference/combined/JAPAN.json"
-DATASET="JAPAN" # EMA, SwissMedic, Japan, Australia"
+INPUT_FILE="inference/combined/with_extracted_data/JAPAN.json"
+DATASET="SWISSMEDIC" # EMA, SWISSMEDIC, JAPAN, AUSTRALIA"
+
+# iterate over the columns of interest
+
+COLUMNS_OF_INTEREST=("Indication_approved" "Marketing_authorisation_holder" "Indication_requested")
+
 # ==========================================================
 
-SAVE_DIR="./inference/combined"
+SAVE_DIR="./inference/combined/with_extracted_data"
 mkdir -p "$SAVE_DIR"
 
 python ./src/extract_from_columns.py\
     --input_file "$INPUT_FILE" \
-    --slice 10 \
-    --model "gpt-4o-mini" \
-    --save_dir "$SAVE_DIR" \
+    --columns_of_interest "${COLUMNS_OF_INTEREST[@]}" \
+    --slice 2 \
+    --model "gpt-4o" \
+    --save_file "$SAVE_DIR/$DATASET.json" \
     --temperature "0.1" \
-    --max_tokens "100" \
+    --max_tokens "1000" \
     --dataset "$DATASET" 
+
+
+echo "Extraction completed. Output saved to $SAVE_DIR/$DATASET.json"
+echo "Converting JSON to CSV..."
+
+python ./src/json_to_csv.py\
+    --input_file "$SAVE_DIR/$DATASET.json" \
+    --output_file "$SAVE_DIR/$DATASET.csv" \
+
+echo "Conversion completed. Output saved to $SAVE_DIR/$DATASET.csv"
