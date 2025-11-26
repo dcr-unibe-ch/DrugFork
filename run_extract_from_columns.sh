6d8c66f093900d4cfad36d68179361ac275beba7
Adferenta <j_dort@outlook.com>
2025-11-26 15:17:29 +0100

    add parent holder extraction for EMA, Swissmedic, Japan, Australia

--- run_extract_from_columns.sh
+++ run_extract_from_columns.sh
@@ -5,9 +5,9 @@ DATASETS=(
     # "JAPAN" 
     # "AUSTRALIA" 
     # "EMA" 
-    # "SWISSMEDIC"
+    "SWISSMEDIC"
     # "FDA"
-    "HEALTHCANADA"
+    # "HEALTHCANADA"
 )
 
 for DATASET in "${DATASETS[@]}"; do
@@ -16,42 +16,49 @@ for DATASET in "${DATASETS[@]}"; do
     # INPUT_FILE="inference/combined/$DATASET.json"
     # INPUT_FILE="data/FDA/$DATASET.json"
     # INPUT_FILE="data/HealthCanada/$DATASET.json"
-    INPUT_FILE="data/HealthCanada/downloads_parsed/parsed.json"
+    # INPUT_FILE="data/HealthCanada/downloads_parsed/parsed.json"
+    # INPUT_FILE="inference/combined/JAPAN_manually_cleaned.json"
+    # INPUT_FILE="inference/combined/EMA_manually_cleaned.json"
+    # INPUT_FILE="inference/combined/AUSTRALIA_manually_cleaned.json"
+    INPUT_FILE="inference/combined/SWISSMEDIC_manually_cleaned.json"
+
+
 
     # iterate over the columns of interest
 
     COLUMNS_OF_INTEREST=(
-        # "Marketing_authorisation_holder"
+        "Marketing_authorisation_holder"
         # "Non_proprietary_name"
-        "Indications_and_usage"
+        # "Indications_and_usage"
     )
 
     # ==========================================================
 
     # SAVE_DIR="./inference/combined/with_extracted_data"
     # SAVE_DIR="data/FDA/with_extracted_data"
-    SAVE_DIR="./data/HealthCanada/with_extracted_data"
-    mkdir -p "$SAVE_DIR"
+    # SAVE_DIR="./data/HealthCanada/with_extracted_data"
+    SAVE_DIR="inference/combined"
+    mkdir -p "$SAVE_DIR"}
 
     python ./src/extract_from_columns.py\
         --input_file "$INPUT_FILE" \
         --columns_of_interest "${COLUMNS_OF_INTEREST[@]}" \
         --slice -1 \
         --model "gpt-4o" \
-        --save_file "$SAVE_DIR/$DATASET.json" \
+        --save_file "$SAVE_DIR/${DATASET}_manually_cleaned.json" \
         --temperature "0.1" \
         --max_tokens "1000" \
         --dataset "$DATASET" 
 
 
-    echo "Extraction completed. Output saved to $SAVE_DIR/$DATASET.json"
+    echo "Extraction completed. Output saved to $SAVE_DIR/${DATASET}_manually_cleaned.json"
     echo "Converting JSON to CSV..."
 
     python ./src/json_to_csv.py\
-         --input_file "$SAVE_DIR/$DATASET.json" \
-         --output_file "$SAVE_DIR/$DATASET.csv" \
+         --input_file "$SAVE_DIR/${DATASET}_manually_cleaned.json" \
+         --output_file "$SAVE_DIR/${DATASET}_manually_cleaned.csv" \
 
-    echo "Conversion completed. Output saved to $SAVE_DIR/$DATASET.csv"
+    echo "Conversion completed. Output saved to $SAVE_DIR/${DATASET}_manually_cleaned.csv"
     
 
 done
